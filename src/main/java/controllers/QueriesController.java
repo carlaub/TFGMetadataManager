3bf8df09cd966c0b6377fefa8a70095b178699ca
carlaub <carlaurreablazquez@gmail.com>
2018-08-02 01:10:39 +0200

    Chained queries

--- src/main/java/controllers/QueriesController.java
+++ src/main/java/controllers/QueriesController.java
@@ -380,15 +380,25 @@ public class QueriesController {
 								if (explorationWithResults == 0) {
 									tempResultQuery.clear();
 								} else {
-									int difference = initialResultQuery.getColumn(j).size() - initialResultQuery.getColumn(j-1).size();
-									System.out.println("Size col j + 1 " + (j+1) + ": " + initialResultQuery.getColumn(j).size() + " ; Size col j " + j + initialResultQuery.getColumn(j-1).size());
-									System.out.println("DIF: " + difference);
+
 									Set<Map.Entry<Integer, ResultEntity>> set = tempResultQuery.entrySet();
 
 									for (Map.Entry<Integer, ResultEntity> result : set) {
-										for (int k = 0; k < difference; k++) {
+//										for (int k = 0; k < difference; k++) {
 											System.out.println("Add entity column: " + result.getKey() + " - " + result.getValue());
 											initialResultQuery.addEntity(initialResultQuery.getColumnsName().indexOf(resultQuery.getColumnsName().get(result.getKey())), result.getValue());
+//										}
+									}
+
+									int difference = initialResultQuery.getColumn(j).size() - initialResultQuery.getColumn(j-1).size();
+									System.out.println("Size col j + 1 " + (j+1) + ": " + initialResultQuery.getColumn(j).size() + " ; Size col j " + j + initialResultQuery.getColumn(j-1).size());
+									System.out.println("DIF: " + difference);
+
+									for (int k = (initialResultQuery.getColumnsCount() - 1); k > 0; k--) {
+										difference = initialResultQuery.getColumn(k).size() - initialResultQuery.getColumn(k-1).size();
+
+										for (int l = 0; l < difference; l++) {
+											initialResultQuery.addEntity(k-1, initialResultQuery.getColumn(k-1).get(initialResultQuery.getColumn(k-1).size()-1));
 										}
 									}
 
