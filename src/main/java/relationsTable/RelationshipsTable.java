edd0de8f4c52b285b58f2f639c08420f03e8de4a
carlaub <carlaurreablazquez@gmail.com>
2018-08-02 19:58:09 +0200

    Chained queries

--- src/main/java/relationsTable/RelationshipsTable.java
+++ src/main/java/relationsTable/RelationshipsTable.java
@@ -51,7 +51,7 @@ public class RelationshipsTable {
 		if (existsRelationship(borderNodeId, idNode1, idNode2)) {
 
 			System.out.println("Count relations: " + countRelationships(borderNodeId, idNode1, idNode2) + " - Fetch: " + fetchedResults);
-			if ( fetchedResults <= countRelationships(borderNodeId, idNode1, idNode2)) {
+			if ( fetchedResults < countRelationships(borderNodeId, idNode1, idNode2)) {
 				return true;
 			}
 		}
